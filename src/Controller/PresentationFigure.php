22cb584c712f05280e83c7c9f45eff400a0808e5
Smeety <smeety.vainqueur@iia-formation.fr>
2023-04-20 13:48:33 +0200

    Ajout des fichiers pour la page de présentation d'une figure

---
+++ src/Controller/PresentationFigure.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PresentationFigure extends AbstractController
+{
+    #[Route('/presentationfigure', name: 'app_figure')]
+    public function index(): Response
+    {
+        return $this->render('pagePresentationFigure/PresentationFigure.html.twig', [
+            'controller_name' => 'FigureController',
+        ]);
+    }
+}
