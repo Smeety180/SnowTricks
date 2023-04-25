ee09d6023bf00027452bb1f4e70e198cf34e3ef6
Smeety <smeety.vainqueur@iia-formation.fr>
2023-04-25 17:11:06 +0200

    RepeatedType Field

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -19,7 +20,9 @@ class RegistrationFormType extends AbstractType
         $builder
             ->add('nom')
             ->add('prenom')
-            ->add('pseudo')
+            ->add('pseudo', null, [
+                'label' => 'Utilisateur',
+            ])
             ->add('photo')
             ->add('email')
             ->add('agreeTerms', CheckboxType::class, [
@@ -30,8 +33,13 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-            ->add('password')
-            ->add('plainPassword', PasswordType::class, [
+
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'The password fields must match.',
+                'required' => true,
+                'first_options'  => ['label' => 'Password'],
+                'second_options' => ['label' => 'Repeat Password'],
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
