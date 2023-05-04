c953b991953b3d1d37722af6655786cbc1a81ce6
Smeety <smeety.vainqueur@iia-formation.fr>
2023-05-04 20:17:56 +0200

    OK

--- src/DataFixtures/FigureFixtures.php
+++ src/DataFixtures/FigureFixtures.php
@@ -26,8 +26,33 @@ class FigureFixtures extends Fixture implements DependentFixtureInterface
         $manager->persist($mute);
         $this->addReference('mute', $mute);
 
-        //        Test premiere figure fixture
+        //        Test deuxieme figure fixture
+        $nose = new Figure();
+        $nose->setNom('nose');
+        $nose->setDescription('Description de la figure nose');
+        $nose->setCategorie($this->getReference('grabs'));
+        $nose->setUser($this->getReference('utilisateur'));
 
+        $noseImage = new Image();
+        $noseImage->setNomDeFichier('nose.png');
+        $nose->addImage($noseImage);
+
+        $manager->persist($nose);
+        $this->addReference('nose', $nose);
+
+        //        Test 3 figure fixture
+        $melon = new Figure();
+        $melon->setNom('melon');
+        $melon->setDescription('Description de la figure melon');
+        $melon->setCategorie($this->getReference('grabs'));
+        $melon->setUser($this->getReference('utilisateur'));
+
+        $melonImage = new Image();
+        $melonImage->setNomDeFichier('melon.pdf');
+        $melon->addImage($melonImage);
+
+        $manager->persist($melon);
+        $this->addReference('melon', $melon);
         $manager->flush();
     }
 
