6e60cf6d17ebae5f2cbe9534d3fcb11eb9473b93
Smeety <smeety.vainqueur@iia-formation.fr>
2023-05-04 20:47:49 +0200

    En cours de fixture commentaire

--- src/DataFixtures/CommentaireFixture.php
+++ src/DataFixtures/CommentaireFixture.php
@@ -1,11 +1,27 @@
 <?php
 
+namespace App\DataFixtures;
 
+use App\Entity\Commentaire;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Datetime;
 
+class CommentaireFixture extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
 
+        $com1 = new Commentaire();
+        $datemsg = new DateTime();
+        $datemsg = DateTime::createFromFormat('d-m-Y H:i:s', '04-05-2023 12:00:00');
+        $com1->setDateMsg($datemsg);
+        $com1->setContenu('il était une fois.....');
+        $this->addReference('mute', $com1);
+        $toto = $this->getReference('utilisateur');
+        $com1->setUser($toto);
 
-
-
-
-
- ?>
+        $manager->persist($com1); // Ajoutez cette ligne pour persister l'objet Commentaire
+        $manager->flush();
+    }
+}
