b86311892921b9474d56999c6e39e2a3610c4d24
Smeety <smeety.vainqueur@iia-formation.fr>
2023-05-11 09:39:17 +0200

    OK

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -41,7 +41,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?string $pseudo = null;
 
     #[ORM\Column(length: 255)]
-    private ?string $photo = null;
+    private ?string $photo = 'LoginSnowboard.png';
 
     #[ORM\OneToMany(mappedBy: 'user', targetEntity: Commentaire::class, orphanRemoval: true)]
     private Collection $commentaires;
